c94fd6a14353ec5b743a7fae2c5977c3002f5bc2
MacGH23 <macgh23@proton.me>
2024-04-14 16:48:45 +0200

    Update TruckiPCB

--- lt232.py
+++ lt232.py
@@ -11,7 +11,8 @@
 ############################################################################
 
 # Controlling the Lumetree devices LT 600 / 1000 / 2000
-# Should also work for Trucki-Platine 
+# Should also work for Trucki-Platine for Lumentree but not tested
+# Should also work for TruckiRS485-Platine for SUN1000 but not tested
 # and also not fully tested.
 # Use at your own risk !  
 
@@ -21,6 +22,7 @@
 
 # Version history
 # macGH 26.11.2023  Version 0.1.0
+# macGH 14.04.2024  Version 0.1.1: Added Trucki RS485 via UART
 
 import os
 import logging
@@ -34,18 +36,43 @@ import minimalmodbus
 # 107 = Read Watt out *0,1
 # 109 = DC Voltage    *0,1
 
+LT_Set_Watt    =  40
+LT_AC_Voltage  =  70
+LT_Temperature =  94
+LT_Read_Watt   = 107
+LT_DC_Voltage  = 109
+
+#[0] 	Set inverter AC output power [W*10] 	(Update rate ~100ms)
+#[1] 	Read display AC output power [W*10] 	(Update rate ~1.3s)
+#[2] 	Read display grid voltage [V*10]    	(Update rate ~1.3s)
+#[3] 	Read display battery [V*10] 	        (Update rate ~1.3s)
+#[4] 	Set / Read DAC Value; [0]=0! 	        (Update rate ~100ms)
+#[5] 	=1 Start Calibration. 17 Steps 	        (10s per step)
+#[6] 	Mirror of REG[0] [W*10] 	            (Update rate ~100ms) Firmware >= 1.06
+#[7] 	Temperature [0..118°C] 	                (Update rate ~100ms) Firmware >= 1.06
+TR_Set_Watt    = 0 
+TR_AC_Voltage  = 2
+TR_Temperature = 7 
+TR_Read_Watt   = 1 
+TR_DC_Voltage  = 3 
+
+
 ######################################################################################
 # Explanations
 ######################################################################################
 
 ######################################################################################
-# def __init__(self, devpath, idadr, loglevel):
+# def __init__(self, devtype, devpath, idadr, loglevel):
+#
+# devtype
+# 0 = Lumentree
+# 1 = Trucki RS485 PCB (https://github.com/trucki-eu/RS485-Interface-for-Sun-GTIL2-1000)
 #
 # devpath
 # Add the /dev/tty device here, mostly .../dev/ttyUSB0, if empty default path /dev/ttyUSB0 is used
 #
 # idadr
-# ID of the lumentree if you have more than one installed, if empty 1 is used
+# ID of the lumentree/TruckiRS485 if you have more than one installed, if empty 1 is used
 #
 # loglevel
 # Enter Loglevel 0,10,20,30,40,50 
@@ -62,8 +89,9 @@ import minimalmodbus
 ##class
 class lt232:
 
-    def __init__(self, devpath, idadr, loglevel):
+    def __init__(self, devtype, devpath, idadr, loglevel):
         #init with default
+        self.devtype = devtype
         self.devpath  = "/dev/ttyUSB0" #just try if is is the common devpath
         self.idadr    = 1              #just try if is is the common idadr
         self.loglevel = 20             #just use info as default
@@ -71,7 +99,21 @@ class lt232:
         if devpath  != "": self.devpath    = devpath
         if idadr    != "": self.idadr      = idadr
         if loglevel != "": self.loglevel   = loglevel
-        
+
+        if(devtype == 0): #Lumentree
+            self.DEV_Set_Watt    = 40 
+            self.DEV_AC_Voltage  = 70
+            self.DEV_Temperature = 94
+            self.DEV_Read_Watt   = 107 
+            self.DEV_DC_Voltage  = 109 
+            
+        if(devtype == 1): #Trucki RS485
+            self.DEV_Set_Watt    = 0 
+            self.DEV_AC_Voltage  = 2
+            self.DEV_Temperature = 7
+            self.DEV_Read_Watt   = 1 
+            self.DEV_DC_Voltage  = 3 
+
         logging.basicConfig(level=loglevel, encoding='utf-8')
         logging.info("Init lt232 class")
 
@@ -80,9 +122,9 @@ class lt232:
         try:
             self.lt232 = minimalmodbus.Instrument(self.devpath, self.idadr) # port name, slave address (in decimal)
         except Exception as e:
-            logging.error("Can not init Lumentree device: " + devpath + " address: " + idadr)
+            logging.error("Can not init Lumentree/TruckiRS485 device: " + devpath + " address: " + idadr)
             logging.error("If device is correct, check if User is in dialout group !")
-            raise Exception("LUMENTREE DEVICE NOT FOUND")
+            raise Exception("LUMENTREE/TRUCKIRS485 DEVICE NOT FOUND")
 
         self.lt232.serial.baudrate = 9600   # Baudrate
         self.lt232.serial.timeout  = 1      # according Spec LT has a 1 Second timeout
@@ -93,8 +135,8 @@ class lt232:
         try:
             self.lt232.serial.close() #Shutdown our interface
         except Exception as e:
-            logging.error("Can not close Lumentree device")
-            raise Exception("LUMENTREE DEVICE CAN NOT BE CLOSED")
+            logging.error("Can not close Lumentree/TruckiRs485 device")
+            raise Exception("LUMENTREE/TRUCKIRS485 DEVICE CAN NOT BE CLOSED")
 
     #############################################################################
     # Read Write operation function
@@ -127,36 +169,38 @@ class lt232:
                 
             return value
 
-    
     #############################################################################
     # Operation function
     
     def set_watt_out(self,val):
         logging.debug("write power out: " + str(val))
-        self.lt232_IO(40,1,val) #does only return 0
+        if(self.devtype == 1): #Trucki RS485
+            val = val*10
+        self.lt232_IO(self.DEV_Set_Watt,1,val) #does only return 0
         return val  #return the same value to signal OK
 
     def readACvoltage(self):
-        logging.debug("read AC voltage of lumentree")
-        rval = self.lt232_IO(70,0,0)
-        logging.debug("AC voltage of lumentree: " + str(rval/10))
+        logging.debug("read AC voltage of lumentree/TruckiRS485")
+        rval = self.lt232_IO(self.DEV_AC_Voltage,0,0)
+        logging.debug("AC voltage of lumentree/TruckiRS485: " + str(rval/10))
         return (rval/10)
 
     def readtemp(self):
-        logging.debug("read temperature of lumentree")
-        rval = self.lt232_IO(94,0,0)
-        logging.debug("Lumentree temperature: " + str(rval/10))
+        logging.debug("read temperature of lumentree/TruckiRS485")
+        rval = self.lt232_IO(self.DEV_Temperature,0,0)
+        logging.debug("Lumentree/TruckiRS485 temperature: " + str(rval/10))
         return (rval/10)
 
     def read_watt_out(self):
         logging.debug("read power out")
-        rval = self.lt232_IO(107,0,0) 
+        rval = self.lt232_IO(self.DEV_Read_Watt,0,0) 
         rvali = int(rval/10)          #return without decimals
-        if(rvali < 10): rvali = 0 #check if lumentree is off <15 = off
+        if(rvali < 10): rvali = 0 #check if lumentree/TruckiRS485 is off <15 = off
         return int(rvali)   
 
     def readDCvoltage(self):
-        logging.debug("read AC voltage of lumentree")
-        rval = self.lt232_IO(109,0,0)
-        logging.debug("DC voltage of lumentree: " + str(rval))
+        logging.debug("read AC voltage of lumentree/TruckiRS485")
+        rval = self.lt232_IO(self.DEV_DC_Voltage,0,0)
+        logging.debug("DC voltage of lumentree/TruckiRS485: " + str(rval))
+        #return without /10 to have the decimals
         return int(rval)
---
+++ lt232test.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+# Controlling the Lumentree 600/1000/2000
+
+# Requirement for using
+# Needed external python modules
+
+# What is missing:
+
+# macGH 26.11.2023  Version 0.2.3
+
+import os
+import sys
+import signal
+import atexit
+from lt232 import *
+
+
+# "" = default = "/dev/ttyUSB00"
+# if you have another device specify here
+#0=Lumentree
+#1=Trucki RS485
+DEVTYPE = 0
+DEVPATH = "" 
+USEDIDADR = 1
+
+# Enter Loglevel 0,10,20,30,40,50 
+# CRITICAL   50
+# ERROR      40
+# WARNING    30
+# INFO       20
+# DEBUG      10
+# NOTSET      0
+LOGLEVEL     = 20
+logtofile    =  0
+logtoconsole =  1
+
+def on_exit():
+    print("CLEAN UP ...")
+    lt.lt232_close()
+    
+def handle_exit(signum, frame):
+    sys.exit(0)
+
+def mwcan_commands():
+    print("")
+    print(" " + sys.argv[0] + " - controlling the Lumentree 600/1000/2000 devices")
+    print("")
+    print(" Usage:")
+    print("        " + sys.argv[0] + " parameter and <value>")
+    print("")
+    print("       setwatt              -- set WATT outout")
+    print("       readwatt             -- read WATT outout")
+    print("       vread                -- read DC voltage")
+    print("       acvread              -- read AC voltage")
+    print("       tempread             -- read power supply temperature")
+    print("")
+    print("       Version 0.1.0 ")
+
+
+#########################################
+# Operation function
+
+
+def vread():
+    # print ("read dc voltage")
+    # Read DC Voltage
+    v = lt.readDCvoltage()
+    print(v)
+    return v
+
+def acvread():
+    # print ("read ac voltage")
+    # Read AC Voltage
+    v = lt.readACvoltage()
+    print(v)
+    return v
+
+def tempread():
+    # print ("read power supply temperature")
+    # Read AC Voltage
+    v = lt.readtemp()
+    print(v)
+    return v
+
+def setwatt(val):
+    # print ("Set output in WATT")
+    # Set output in Watt
+    
+    v = lt.set_watt_out(val) 
+    return v
+
+def readwatt():
+    # print ("Set output in WATT")
+    # Set output in Watt
+    
+    v = lt.read_watt_out() 
+    print(v)
+    return v
+
+def command_line_argument():
+    if len (sys.argv) == 1:
+        print ("")
+        print ("Error: First command line argument missing.")
+        mwcan_commands()
+        error = 1
+        return
+    
+    if sys.argv[1]   in ['vread']    : vread()
+    elif sys.argv[1] in ['acvread']  : acvread()
+    elif sys.argv[1] in ['tempread'] : tempread()
+    elif sys.argv[1] in ['setwatt']  : setwatt(int(sys.argv[2]))
+    elif sys.argv[1] in ['readwatt'] : readwatt()
+    else:
+        print("")
+        print("Unknown first argument '" + sys.argv[1] + "'")
+        mwcan_commands()
+        error = 1
+        return
+
+#### Main 
+atexit.register(on_exit)
+signal.signal(signal.SIGTERM, handle_exit)
+signal.signal(signal.SIGINT, handle_exit)
+
+mylogs = logging.getLogger()
+mylogs.setLevel(LOGLEVEL)
+
+if logtofile == 1:
+    file = logging.FileHandler(self.logpath, mode='a')
+    file.setLevel(LOGLEVEL)
+    fileformat = logging.Formatter("%(asctime)s:%(module)s:%(levelname)s:%(message)s",datefmt="%H:%M:%S")
+    file.setFormatter(fileformat)
+    mylogs.addHandler(file)
+
+if logtoconsole == 1:
+    stream = logging.StreamHandler()
+    stream.setLevel(LOGLEVEL)
+    streamformat = logging.Formatter("%(asctime)s:%(module)s:%(levelname)s:%(message)s",datefmt="%H:%M:%S")
+    stream.setFormatter(streamformat)    
+    mylogs.addHandler(stream)
+
+
+lt = lt232(DEVTYPE, DEVPATH,USEDIDADR,LOGLEVEL)
+lt.lt232_open()
+
+command_line_argument()
+
+sys.exit(0)
